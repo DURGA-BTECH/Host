c13e719e726ac75b1acc824609470244f864bb0b
DURGA-BTECH <durgas0065@gmail.com>
2025-08-09 21:15:22 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function validateForm() {
+  const password = document.getElementById("password").value;
+  const confirm = document.getElementById("confirmPassword").value;
+  
+  if (password !== confirm) {
+    alert("Passwords do not match!");
+    return false;
+  }
+
+  return true;
+}
